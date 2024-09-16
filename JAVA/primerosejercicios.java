cb68e8ab806f39d9755550357f3ea7736fe3ad1b
Intissar <baloukintissar@gmail.com>
2024-09-16 09:53:22 +0200

    segundo commit

---
+++ JAVA/primerosejercicios.java
@@ -0,0 +1,36 @@
+Actividad 1: Realiza un programa Java que muestre los ficheros de un directorio. El nombre
+del directorio se pasará al programa desde la línea de comandos al ejecutarlo
+
+Actividad 2: Crea un fichero de texto con algún editor de textos y después realiza un programa
+Java que visualice su contenido. Cambia el programa Java para que el nombre del fichero se
+acepte al ejecutar desde la línea de comandos
+
+
+Actividad 3: Crea un programa Java que reciba un identificador de empleado desde la línea de
+comandos y visualice sus datos. Si el empleado no existe debe visualizar mensaje indicándolo.
+
+Actividad 4: Crea un programa Java que reciba desde la línea de comandos un identificador
+de empleado y un importe. Se debe sumar al salario del empleado el importe tecleado. El
+programa debe visualizar el apellido, el salario antiguo y el nuevo. Si el identificador no existe
+se visualizará un mensaje indicándolo
+
+Actividad 5: A partir del fichero de objetos Persona utilizado anteriormente, crea un documento XML
+usando DOM.
+
+Actividad 6: Utiliza SAX para visualizar el contenido del fichero Empleados.xml creado
+anteriormente.
+
+1 Realiza un programa Java que cree un fichero binario para guardar datos de departamentos, dale
+el nombre Departamentos.dat Introduce varios departamentos. Los datos por cada departamento
+son;
+Número de departamento: entero. Nombre: String. Localidad: String.
+2 Realiza un programa Java que te permita modificar los datos de un departamento. El programa
+recibe desde la línea de comandos el número de departamento a modificar, el nuevo nombre de
+departamento y la nueva localidad. Si el departamento no existe visualiza un mensaje
+indicándolo. Visualiza también los datos antiguos del departamento y los nuevos datos.
+3 Realiza un programa Java que te permita eliminar un departamento. El programa recibe desde la
+línea de comandos el número de departamento a eliminar. Si el departamento no existe visualiza
+un mensaje indicándolo. Visualiza también el número total de departamentos que existen en el
+fichero.
+4 Realiza un programa que copie dos ficheros. El nombre de los dos ficheros, origen y destino, se
+introducen desde la línea de comandos al ejecutar el programa.
